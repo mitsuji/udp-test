475b5de15d6788d8fd6f1c6d02d1f40601bd96c3
mitsuji <tkms@mitsuji.org>
2019-04-18 17:54:00 +0900

    clinet recv

--- app/client/Main.hs
+++ app/client/Main.hs
@@ -1,22 +1,29 @@
+
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
 import System.Environment (getArgs)
-import Network.Socket hiding (sendAllTo)
-import Network.Socket.ByteString (sendAllTo)
+import Network.Socket hiding (sendAllTo,recv)
+import Network.Socket.ByteString (sendAllTo,recv)
 import Data.Monoid((<>))
 
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
+import qualified Data.ByteString as B
 
 main :: IO ()
 main = do
   host:port:message:_ <- getArgs
   withSocketsDo $ do
-    addr <- resolveServer host port
-    sock <- open' addr
-    sendAllTo sock (T.encodeUtf8 $ T.pack message) (addrAddress addr)
+    serverAddr <- resolveServer host port
+    localAddr <- resolveServer "0.0.0.0" port
+    sock <- bind' localAddr
+    sendAllTo sock (T.encodeUtf8 $ T.pack $ message) (addrAddress serverAddr)
+    bs <- recv sock 1024
+    close sock
+    B.putStr bs
+    B.putStrLn ""
 
       
 resolveServer host port = do
@@ -26,8 +33,10 @@ resolveServer host port = do
   addr:_ <- getAddrInfo (Just hints) (Just host) (Just port)
   return addr
 
-
-open' addr = do
+  
+bind' addr = do
   sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
+  setSocketOption sock ReuseAddr 1
+  bind sock (addrAddress addr)
   return sock
   
